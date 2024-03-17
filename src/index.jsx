c272f35bd49dd20db3a7753e6f7fb32b3e99181c
Madi <havkpzz@gmail.com>
2024-03-17 13:43:49 +0600

    Created function for generating items insied desktop content

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,72 @@
 import "./styles/main.css";
-import "./assets/images/artage-io-48140_1710064888.png";
+import "./assets/images/note.png";
+import "./assets/images/computer.png";
+import "./assets/images/disc.png";
 
-const icon = document.getElementById("resume");
+const desktopLabels = [
+  {
+    name: "note",
+    title: "resume.doc",
+  },
+  {
+    name: "computer",
+    title: "My Computer",
+  },
+  {
+    name: "disc",
+    title: "disc",
+  },
+];
+const desktopContent = document.querySelector(".desktop-content");
 
-function moveDesktopLabel(moveEvent) {
-  const outOfScreenY =
-    moveEvent.clientY > window.innerHeight || moveEvent.clientY < 0;
-  const outOfScreenX =
-    moveEvent.clientX > window.innerWidth || moveEvent.clientX < 0;
-  if (outOfScreenY || outOfScreenX) {
-    document.removeEventListener("mousemove", moveDesktopLabel);
-  }
-  icon.style.top = `${moveEvent.clientY - 25}px`;
-  icon.style.left = `${moveEvent.clientX - 40}px`;
+function createDesktopLabels(labels) {
+  let positionY = 20;
+
+  labels.forEach((label) => {
+    if (desktopContent.clientHeight < positionY) return;
+
+    const desktopLabel = document.createElement("div");
+    desktopLabel.className = "desktop-label";
+    desktopLabel.id = label.name;
+    desktopLabel.style.top = `${positionY}px`;
+    positionY += 100;
+
+    const desktopLabelIcon = document.createElement("img");
+    desktopLabelIcon.src = `./assets/images/${label.name}.png`;
+    desktopLabelIcon.alt = label.name;
+    desktopLabelIcon.className = "desktop-label-icon";
+    desktopLabelIcon.draggable = false;
+
+    const desktopLabelTitle = document.createElement("p");
+    desktopLabelTitle.textContent = label.title;
+    desktopLabelTitle.className = "desktop-label-title";
+
+    desktopLabel.appendChild(desktopLabelIcon);
+    desktopLabel.appendChild(desktopLabelTitle);
+    desktopContent.appendChild(desktopLabel);
+  });
 }
+createDesktopLabels(desktopLabels);
+const icon = document.getElementById("note");
 
-icon.addEventListener("mousedown", (downEvent) => {
-  icon.style.top = `${downEvent.clientY - 25}px`;
-  icon.style.left = `${downEvent.clientX - 40}px`;
-  document.addEventListener("mousemove", moveDesktopLabel);
-});
-icon.addEventListener("mouseup", (upEvent) => {
-  icon.style.top = `${upEvent.clientY - 50}px`;
-  icon.style.left = `${upEvent.clientX - 50}px`;
-  document.removeEventListener("mousemove", moveDesktopLabel);
-});
+function moveDesktopItems(event) {
+  const outOfScreenY = event.clientY > window.innerHeight || event.clientY < 0;
+  const outOfScreenX = event.clientX > window.innerWidth || event.clientX < 0;
+  if (outOfScreenY || outOfScreenX) {
+    document.removeEventListener("mousemove", moveDesktopItems);
+  }
+  icon.style.top = `${event.clientY - 25}px`;
+  icon.style.left = `${event.clientX - 40}px`;
+}
+function actionMouseDown(event) {
+  icon.style.top = `${event.clientY - 25}px`;
+  icon.style.left = `${event.clientX - 40}px`;
+  document.addEventListener("mousemove", moveDesktopItems);
+}
+function actionMouseUp(event) {
+  icon.style.top = `${event.clientY - 50}px`;
+  icon.style.left = `${event.clientX - 50}px`;
+  document.removeEventListener("mousemove", moveDesktopItems);
+}
+icon.addEventListener("mousedown", actionMouseDown);
+icon.addEventListener("mouseup", actionMouseUp);
